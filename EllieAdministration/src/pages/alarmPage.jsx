a6d3ea23c419e0196bc2af4d94f9d85437e0de9d
Minhly <minh.y.ly@hotmail.com>
2024-04-22 08:25:56 +0200

    Changes to list of alarms with user now

--- EllieAdministration/src/pages/alarmPage.jsx
+++ EllieAdministration/src/pages/alarmPage.jsx
@@ -32,6 +32,7 @@ function createData(id, name, activateAlarm, description, image) {
 
 function AlarmPage() {
   const [alarms, setAlarms] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
   const bearerToken = useLoggedInStore((state) => state.bearerToken);
 
   const config = {
@@ -41,7 +42,7 @@ function AlarmPage() {
     },
   };
 
-  const url = "https://deep-wealthy-roughy.ngrok-free.app/alarm";
+  const url = "https://deep-wealthy-roughy.ngrok-free.app/alarm/WithUsers";
   useEffect(() => {
     axios
       .get(url, config)
@@ -50,6 +51,27 @@ function AlarmPage() {
       })
       .catch((err) => console.log(err));
   }, []);
+
+  useEffect(() => {
+    axios
+      .get(url, config)
+      .then((res) => {
+        setFilteredList(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const filterBySearch = (event) => {
+    const query = event.target.value;
+    var updatedList = [...alarms];
+    updatedList = updatedList.filter((item) => {
+      return (
+        (item.user.lastName || "").toLowerCase().includes(query) ||
+        (item.user.firstName || "").toLowerCase().includes(query)
+      );
+    });
+    setFilteredList(updatedList);
+  };
 
   createData(alarms);
   return (
@@ -83,7 +105,18 @@ function AlarmPage() {
         >
           <CreateAlarmModal />
         </div>
-        <TableContainer style={{ maxHeight: 600 }} component={Paper}>
+        <TableContainer style={{ maxHeight: 800 }} component={Paper}>
+          <TextField
+            id="search-box"
+            label="Filtrere efter Beboer"
+            onChange={filterBySearch}
+            style={{
+              marginBottom: "20px",
+              float: "left",
+              marginLeft: "50px",
+              marginTop: "20px",
+            }}
+          />
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
               <TableRow style={{ backgroundColor: "#f5f5f5", height: "35px" }}>
@@ -115,13 +148,19 @@ function AlarmPage() {
                   align="left"
                   sx={{ fontWeight: "bold", color: "#85B585" }}
                 >
+                  Beboer
+                </TableCell>
+                <TableCell
+                  align="left"
+                  sx={{ fontWeight: "bold", color: "#85B585" }}
+                >
                   Alarm ringer
                 </TableCell>
                 <TableCell align="left"></TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {alarms.map((row) => (
+              {filteredList.map((row) => (
                 <TableRow
                   key={row.id}
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -132,6 +171,9 @@ function AlarmPage() {
                   <TableCell align="left">{row.id}</TableCell>
                   <TableCell align="left">{row.name}</TableCell>
                   <TableCell align="left">{row.description}</TableCell>
+                  <TableCell align="left">
+                    {row.user.firstName} {row.user.lastName}
+                  </TableCell>
                   <TableCell align="left">{row.activatingTime}</TableCell>
                   <TableCell align="left">
                     <EditAlarmModal alarm={row} />
